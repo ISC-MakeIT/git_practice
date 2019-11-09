24e2488413d9fb6f89318a4705936da5ea865a12
tttkouyou <bjkt1290248@gn.iwasaki.ac.jp>
2019-11-09 18:00:04 +0900

    class_nameだったところをtaskNumberに置換しました

--- Todo/src/tagStatusChange.js
+++ Todo/src/tagStatusChange.js
@@ -1,47 +1,47 @@
 const completeFnc = () => {
   //移動先の子要素にコピペした後に、移動前の要素を削除する
-  document.getElementById("end").appendchild(class_name[0]);
-  document.getElementById("doing").removeChild(class_name[0]);
+  document.getElementById("end").appendchild(taskNumber[0]);
+  document.getElementById("doing").removeChild(taskNumber[0]);
 
   //[1]以外の[0]の子要素を削除する
   for (let i = 2; i <= 6; i++) {
-    class_name[0].removeChild(class_name[i]);
+    taskNumber[0].removeChild(taskNumber[i]);
   }
 };
 
 const deleteFnc = () => {
   //子要素から親要素を取得
-  const parent = document.getElementsByClassName("class_name")[0].parentNode;
+  const parent = document.getElementsByClassName("taskNumber")[0].parentNode;
 
   //取得した親要素を使ってタスクをdarkに移動させる
-  document.getElementById("dark").appendChild(class_name[0]);
-  parent.removeChild(class_name[0]);
+  document.getElementById("dark").appendChild(taskNumber[0]);
+  parent.removeChild(taskNumber[0]);
 };
 
 const interruptionFnc = () => {
   //要素を移動させる
-  document.getElementById("break").appendchild(class_name[0]);
-  document.getElementById("going").removeChild(class_name[0]);
+  document.getElementById("break").appendchild(taskNumber[0]);
+  document.getElementById("going").removeChild(taskNumber[0]);
 
   //必要のないボタンを見せないようにCSSを変える
   document.getElementsByClassName(
-    "class_name"
+    "taskNumber"
   )[3] /*完了ボタン*/.getElementsByClassName.style.display = "none";
   document.getElementsByClassName(
-    "class_name"
+    "taskNumber"
   )[5] /*中断ボタン*/.getElementsByClassName.style.display = "none";
 };
 
 const resumeFnc = () => {
   //再びボタン機能を視覚化するためにCSSを変更する
   document.getElementsByClassName(
-    "class_name"
+    "taskNumber"
   )[3] /*完了ボタン*/.getElementsByClassName.style.display = "inline";
   document.getElementsByClassName(
-    "class_name"
+    "taskNumber"
   )[5] /*中断ボタン*/.getElementsByClassName.style.display = "inline";
 
   //要素を移動させる
-  document.getElementById("going").appendchild(class_name[0]);
-  document.getElementById("break").removeChild(class_name[0]);
+  document.getElementById("going").appendchild(taskNumber[0]);
+  document.getElementById("break").removeChild(taskNumber[0]);
 };
